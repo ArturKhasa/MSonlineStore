cbdec9f40af69df323f0e476b69985eb3939a18d
arturhasahanov <arturbk2@yandex.ru>
2022-09-18 20:44:11 +0300

    Main commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLoginForm(){
+        //
+    }
+
+    public function login(){
+        //
+    }
+
+    public function showRegisterForm(){
+        //
+    }
+
+    public function register(){
+        //
+    }
+}
